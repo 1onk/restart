c7feaf0b524b7efdb2cc3f14a4498c938c871b74
1onk <406061841@qq.com>
2021-04-21 01:30:31 +0800

    1.go

---
+++ leetcode/1-100/1.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for index, value := range nums {
+        if idx, ok := m[target-value]; ok {
+            return []int{idx, index}
+        }
+        m[value] = index
+    }
+    return nil
+}
